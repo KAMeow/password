7ce022eeb3d04ec404c9480cfd80db7e47ab7faa
Katrina <katrinayu89@gmail.com>
2020-06-10 21:16:06 +0800

    update

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 x = 3 #剩余机会
 while x > 0:
+	x = x - 1
 	q = input('请输入你的密码: ')
 	if q == password:
 		print('登陆成功!')
 		break
 	else:
-		x = x - 1
-		print('密码错误! 还有', x, '次机会')
+		print('密码错误!')
+		if x > 0:
+			print('还有', x, '次机会')
+		else:
+			print('没机会尝试了！要锁账号了!')
